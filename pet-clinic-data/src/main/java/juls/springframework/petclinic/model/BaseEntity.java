fcde7e1bd7e02e3127916c1e0aaa627be0db1934
jmakalanda <jmakalanda@gmail.com>
2020-06-28 17:56:06 +0200

    Adding base entity. Closes #10

---
+++ pet-clinic-data/src/main/java/juls/springframework/petclinic/model/BaseEntity.java
@@ -0,0 +1,15 @@
+package juls.springframework.petclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+    private Long Id;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+}
